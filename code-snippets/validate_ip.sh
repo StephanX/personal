264706896e75b448b41c53fcf8bd6d12407006f5
Stephan Budge <stephan@stephanx.net>
2013-03-14 11:15:24 -0700

    tweaking validate_ip

--- code-snippets/validate_ip.sh
+++ code-snippets/validate_ip.sh
@@ -25,6 +25,28 @@ function valid_ip()
     return $stat
 }
 
+# If run directly, execute some tests.
+if [[ -z $1 ]]; then
+    ips='
+        4.2.2.2
+        a.b.c.d
+        192.168.1.1
+        0.0.0.0
+        255.255.255.255
+        255.255.255.256
+        192.168.0.1
+        192.168.0
+        1234.123.123.123
+        '
+    for ip in $ips
+    do
+        if valid_ip $ip; then stat='good'; else stat='bad'; fi
+        printf "%-20s: %s\n" "$ip" "$stat"
+    done
+    exit 0
+fi
+
+# Actually validate if input is given, exit 1 if validation fails
 for ip in $1
 do
     if valid_ip $ip; then stat='good'; else stat='bad'; fi
